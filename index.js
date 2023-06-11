f92b77f2d1cb79a6ed4dff9ad6a79cb7df7e4faa
mbron770 <mbronfin.tsd@gmail.com>
2023-06-11 17:14:09 -0400

    add array methods

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(driver => driver.indexOf(name) === 0)
+}
+
+function matchName(drivers, str){
+    return drivers.filter(driver => driver.name === str)
+}
+
+
+
+
+
